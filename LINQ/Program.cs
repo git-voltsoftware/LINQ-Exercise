f25cc52e9cfec83f930be2a60e7bfdca2df92cff
git-voltsoftware <info@volt.software>
2023-05-07 18:57:58 -0400

    finish method syntax

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace LINQ
+﻿using System;
+using System.Linq;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGames = new List<string>
+            {
+                "Super Mario Bros.",
+                "The Legend of Zelda",
+                "Sonic the Hedgehog",
+                "Mega Man",
+                "Street Fighter II",
+                "Final Fantasy VII",
+                "Pokemon Red/Blue",
+                "GoldenEye 007",
+                "Resident Evil 4",
+                "Grand Theft Auto V"
+            };
+
+            var sortedGames = videoGames.OrderBy(game => game);
+
+            foreach (var game in sortedGames)
+            {
+                Console.WriteLine(game);
+            }
+
+
+            // Lambda expression 
+
+            var numGamesLambda = videoGames.Count(game => true);
+
+            Console.WriteLine($"Number of video games in this list: {numGamesLambda}");
         }
     }
 }
